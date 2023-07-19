cd300b81755954b1e7e8cbf297ce4f83dec5abb3
Tadidivyaa <126139531+Tadidivyaa@users.noreply.github.com>
2023-07-19 12:06:41 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,7 @@
+function on(){
+    document.getElementById("flower").src="https://housing.com/news/wp-content/uploads/2022/11/blossom-flower-compressed.jpg"
+
+}
+function off(){
+    document.getElementById("flower").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW2Bd0CXpjIZrEMtuB1wk1CI0gFGo32iiX3g&usqp=CAU";
+}
